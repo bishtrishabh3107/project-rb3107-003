5b34c48a27ef6d1725bfe629f58b367d5294d318
Rishabh Bisht <bishtrishabh3107@gmail.com>
2021-01-23 17:29:16 +0530

    changed icon path

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: '../CodeFolder/src/assets/images/icon.png',
+        icon: 'src/assets/images/icon.pngs',
       },
     },
     {
